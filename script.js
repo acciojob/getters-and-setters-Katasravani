0b94eee005935a8cca02a270462516a102d853d7
Katasravani <105917453+Katasravani@users.noreply.github.com>
2023-12-12 11:40:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
-//complete this code
-class Person {}
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-class Student extends Person {}
+  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  set age(age) {
+    this._age = age;
+  }
+}
 
-// Do not change the code below this line
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
+class Student extends Person {
+  constructor(name, age) {
+    super(name, age);
+  }
+
+  study() {
+    console.log(`${this.name} is studying.`);
+  }
+}
+
+class Teacher extends Person {
+  constructor(name, age) {
+    super(name, age);
+  }
+
+  teach() {
+    console.log(`${this.name} is teaching.`);
+  }
+}
+
+// Example usage
+const student = new Student("John", 20);
+const teacher = new Teacher("Professor Smith", 35);
+
+student.study(); // Output: John is studying.
+teacher.teach(); // Output: Professor Smith is teaching.
